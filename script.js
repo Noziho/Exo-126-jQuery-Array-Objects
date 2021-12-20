0e15bb054bb9e3b1356794193fdd67c11bf4f310
Noah2211 <noah.decroix3@gmail.com>
2021-12-20 20:00:30 +0100

    start exercice

--- script.js
+++ script.js
@@ -5,27 +5,14 @@ let arrObj = [
     {"person" : "Spencer", "age" : 34, "avatar" : "https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraight&accessoriesType=Prescription02&hairColor=Blonde&facialHairType=BeardMedium&facialHairColor=Black&clotheType=CollarSweater&clotheColor=PastelRed&eyeType=Dizzy&eyebrowType=SadConcerned&mouthType=Sad&skinColor=Yellow"}
 ];
 
-//Methode js native pour parcourir un tableau d'objets ( avec for...in )
-for(let i in arrObj) {
-    // i est l'index de mon tableau d'objet
-    console.log("index = " + i);
-
-    // j'accède à mon tableau d'objet comme n'importe quel tableau
-    console.log("arrObj = " + arrObj[i]);
-
-    // J'accéde à une propriété particuliére de mon objet ,ici l'age
-    console.log("age = " + arrObj[i].age);
-}
-
-//Autre méthode : utilisation de forEach
-arrObj.forEach(function(arr) {
-    //Afficher la propriété age pour chaque objet contenu dans mon tableau
-   console.log("Resultat avec foreach = " + arr.age);
-});
 
 //Méthode utilisant jQuery
 $.each(arrObj, function(index, value) {
     //Afficher la propriété age pour chaque objet contenu dans mon tableau
-   console.log("Affichage age utilisant jQuery = " + value.age);
+   const createImg = document.createElement('img');
+   createImg.setAttribute('href', value.avatar);
+   createImg.style.width = "200px";
+
+   $('body').append(createImg);
 });
 
